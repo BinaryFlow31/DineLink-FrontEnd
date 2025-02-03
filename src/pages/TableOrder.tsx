f3fc4036604d5308ab6790453b18ab35e01eccf5
Anushk Jain <anushk2201@gmail.com>
2025-02-03 21:55:38 +0530

    bug resolved

--- src/pages/TableOrder.tsx
+++ src/pages/TableOrder.tsx
@@ -1,8 +1,12 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
+import HowWeWork from "@/components/HowWeWork";
 
 const TableOrder = () => {
 	return <>
         <Header />
+        <Banner />
+        <HowWeWork />
     </>;
 };
 
